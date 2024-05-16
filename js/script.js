f406f45f5a8a2e9dc5d2db3521f71f6cf8114141
Antonino <drakon9927@libero.it>
2024-05-16 19:54:46 +0200

    layout

--- js/script.js
+++ js/script.js
@@ -3,21 +3,14 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            emails: [],  
+              
         }
     },
     methods: {
        
     },
     mounted() {
-        console.log("App montata");
+       
 
-        // Ciclo per ottenere 10 email
-        for (let i = 0; i < 10; i++) {
-            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-                .then(response => {
-                    this.emails.push(response.data.response);  
-                })
-        }
     }
 }).mount(`#app`)
